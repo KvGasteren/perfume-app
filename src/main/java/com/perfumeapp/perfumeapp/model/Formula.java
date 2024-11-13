9ce96bd9530a56263ff76634d5112ac8769fe04c
KvGasteren <gasteren@gmail.com>
2024-11-13 11:56:20 +0100

    add getter/setter name

--- src/main/java/com/perfumeapp/perfumeapp/model/Formula.java
+++ src/main/java/com/perfumeapp/perfumeapp/model/Formula.java
@@ -11,4 +11,12 @@ public class Formula {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
