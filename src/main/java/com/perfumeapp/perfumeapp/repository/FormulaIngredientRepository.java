2e0828b7695b4382d2e9a73b77f3db248ae5859a
KvGasteren <gasteren@gmail.com>
2024-11-15 10:02:37 +0100

    Add Repos

---
+++ src/main/java/com/perfumeapp/perfumeapp/repository/FormulaIngredientRepository.java
@@ -0,0 +1,7 @@
+package com.perfumeapp.perfumeapp.repository;
+
+import com.perfumeapp.perfumeapp.model.FormulaIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormulaIngredientRepository extends JpaRepository<FormulaIngredient, Long> {
+}
