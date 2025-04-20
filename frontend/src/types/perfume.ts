51dba69f76cca3694c35e797ad9e094ef8246c79
Koen van Gasteren <gasteren@gmail.com>
2025-04-20 14:46:02 +0200

    add perfume types

---
+++ frontend/src/types/perfume.ts
@@ -0,0 +1,20 @@
+export interface Allergen {
+  id: number;
+  name: string;
+  concentration: number;
+  maxConcentration: number;
+}
+
+export interface Ingredient {
+  id: number;
+  name: string;
+  parts: number;
+  concentration: number;
+  allergens: Allergen[];
+}
+
+export interface Formula {
+  id: number;
+  name: string;
+  ingredients: Ingredient[];
+}
