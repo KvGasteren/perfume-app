bd04ff9a4b472ca7eb2203fa761fda93499fe53f
LAPTOP_KOEN\Koen <koen.vangasteren@tangelo.nl>
2025-04-23 16:08:08 +0200

    adjust tale style ingredients

--- frontend/src/pages/ingredients/index.tsx
+++ frontend/src/pages/ingredients/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import IngredientList from "../../components/IngredientList";
-import IngredientForm from "../../components/IngredientForm";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
 
 const IngredientsPage: React.FC = () => {
     return (
         <div className="p-4">
-            <h1 className="text-2xl font-bold">Ingredients</h1>
-            <div className="my-4">
-                <IngredientForm />
-            </div>
-            <IngredientList />
+          <h1 className="text-2xl font-bold">Ingredients</h1>
+          <IngredientList />
+          <div className="pr-4 flex justify-end mt-4">
+            <button /*onClick={() => setShowModal(true)} aria-label="Add Allergen"*/ >
+              <PlusCircleIcon className="h-6 w-6 text-primary hover:text-primary/70 cursor-pointer" />
+            </button>
+          </div>
+    
+          {/* <AddAllergenModal
+            isOpen={showModal}
+            onClose={() => setShowModal(false)}
+            onAdd={handleAddAllergen}
+          /> */}
         </div>
-    );
+      )
 };
 
 export default IngredientsPage;
